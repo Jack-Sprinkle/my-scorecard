f01ee173eeaa74d55b78a283239190b7b48c7942
Jack Sprinkle <sprinkle.jas@gmail.com>
2025-03-14 15:26:22 -0400

    fix styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-default">
-          <Header/>
-        <main className="bg-gray-300 h-screen">{children}</main>
+      <body className="font-default flex flex-col min-h-screen bg-gray-200">
+        <Header/>
+        <main className="flex-grow">{children}</main>
         <Footer/>
       </body>
     </html>
